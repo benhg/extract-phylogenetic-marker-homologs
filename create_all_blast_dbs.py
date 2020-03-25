b106a2d077035b6ccf003766a2e2cb7b267dff75
Ben Glick <glick@glick.cloud>
2020-03-25 11:57:37 -0700

    tabs -> spaces

--- create_all_blast_dbs.py
+++ create_all_blast_dbs.py
@@ -3,19 +3,19 @@ from Bio import seqio
 
 @bash_app
 def create_db(sequence, sequence_name):
-	import os
-	os.makedirs(sequence_name)
-	make_sequence_db_input_fasta(sequence, sequence_name)
-	return ""
+    import os
+    os.makedirs(sequence_name)
+    make_sequence_db_input_fasta(sequence, sequence_name)
+    return ""
 
 def make_sequence_db_input_fasta(sequence, sequence_name):
-	import os
-	from glob import glob
-	min_length = 0.8 * len(sequence)
-	with open(f"{sequence_name}/sufficient_length.fasta", "w") as fh:
-		for file in glob("/home/labs/binford/raw_reads/lane*"):
-			with open(file) as read_fh:
-				
+    import os
+    from glob import glob
+    min_length = 0.8 * len(sequence)
+    with open(f"{sequence_name}/sufficient_length.fasta", "w") as fh:
+        for file in glob("/home/labs/binford/Assembled_Untranslated_Transcriptomes/s*"):
+            with open(file) as read_fh:
 
 
-with open("input_sequences.fasta") as fh:
+if __name__ == '__main__':
+    with open("input_sequences.fasta") as fh:
